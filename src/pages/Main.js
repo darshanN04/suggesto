4d4a1684c3ffcd0da7fe11c36a51a64ba8fa6b95
darshanN04 <darshannakshatri@gmail.com>
2024-07-04 14:05:21 +0530

    main-page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/main.css';
-import { FcLike } from "react-icons/fc";
 
 const Main = () => {
   const [comment, setComment] = useState('');
